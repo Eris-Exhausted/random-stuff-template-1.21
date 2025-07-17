929f42713fc2ab2dbac0f8dea562899737d7b433
Eris Flanagan <capcon432@gmail.com>
2025-07-17 12:04:09 -0700

    First commit, changed nothing

---
+++ src/main/java/net/eris/randomstuff/RandomStuff.java
@@ -0,0 +1,16 @@
+package net.eris.randomstuff;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RandomStuff implements ModInitializer {
+	public static final String MOD_ID = "randomstuff";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
