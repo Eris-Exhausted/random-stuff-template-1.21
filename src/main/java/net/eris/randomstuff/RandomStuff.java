d5f4c8a5d0d0911058ed921eae405422218f89f0
Eris <capcon432@gmail.com>
2025-07-18 21:45:04 -0700

    Fixed some mushroom stuff

--- src/main/java/net/eris/randomstuff/RandomStuff.java
+++ src/main/java/net/eris/randomstuff/RandomStuff.java
@@ -4,6 +4,8 @@ import net.eris.randomstuff.block.ModBlocks;
 import net.eris.randomstuff.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //random comment
@@ -13,6 +15,7 @@ public class RandomStuff implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.POISON_MUSHROOM_BLOCK, RenderLayer.getCutout());
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
