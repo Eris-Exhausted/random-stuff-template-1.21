00ed7c069fe52acb8dff8d9fbc335fa2d751d8f9
Eris <capcon432@gmail.com>
2025-07-18 20:13:22 -0700

    Started adding the mushroom as a block

--- src/main/java/net/eris/randomstuff/RandomStuff.java
+++ src/main/java/net/eris/randomstuff/RandomStuff.java
@@ -1,5 +1,6 @@
 package net.eris.randomstuff;
 
+import net.eris.randomstuff.block.ModBlocks;
 import net.eris.randomstuff.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class RandomStuff implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/eris/randomstuff/block/ModBlocks.java
@@ -0,0 +1,44 @@
+package net.eris.randomstuff.block;
+
+import net.eris.randomstuff.RandomStuff;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    public static final Block POISON_MUSHROOM_BLOCK = registerBlock( "poison_mushroom_block",
+            new Block(AbstractBlock.Settings.create()
+                    .nonOpaque()
+                    .breakInstantly()
+                    .sounds(BlockSoundGroup.GRASS)
+                    .noCollision()
+                    .pistonBehavior(PistonBehavior.DESTROY)));
+
+    private static Block registerBlock(String name, Block block){
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(RandomStuff.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(RandomStuff.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        RandomStuff.LOGGER.info("Registering mod blocks for " + RandomStuff.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.POISON_MUSHROOM_BLOCK);
+        });
+    }
+
+}
